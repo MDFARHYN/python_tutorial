b1e052eefb69c5c7b6c06f4c5f82fb6a07b4662b
farhyn.com <info@farhyn.com>
2022-07-09 02:35:20 +0600

    updated

--- tu02/tu02.py
+++ tu02/tu02.py
@@ -1,2 +1,7 @@
-Text_body = "hello world"
-print(Text_body)
+text_Body_show = "Hello world"
+print(text_Body_show)
+# Try to use meaningful name for variable
+# variable is case sensitive
+# Variable can be start with capital letter or small letter or underscore
+# Variable can't be start with number
+# We can't use space in variable
