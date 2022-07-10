3290ad64778347d06272840f7fee595c5d79c0af
farhyn.com <info@farhyn.com>
2022-07-11 05:40:18 +0600

    updated

--- tu03/tu03.py
+++ tu03/tu03.py
@@ -1,1 +1,16 @@
-#numbres, string, list, tuple, dictionary, boolean
+# python basic data types: number,string,Boolean,list,tuple
+# 1) number : 1)interger 2) float 3)complex
+
+
+int_example = 26
+print(type(int_example))
+
+float_example = 26.25
+print(type(float_example))
+
+complex_example = 26j
+print(type(complex_example))
+
+a = 25
+b = 5
+print(a/b)
